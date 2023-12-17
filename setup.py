e82e5bb766f9767194316a9404138835e2785f26
abdulrehman192 <mr.abdulrehman.ar@gmail.com>
2023-12-17 14:46:06 +0500

    add setup file

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'cv2',
-        'mediapipe',
-        'numpy',
-        'skimage',
-        'moviepy',
+        'opencv-contrib-python==4.8.1.78',
+        'mediapipe==0.10.9',
+        'numpy==1.26.2',
+        'scikit-image==0.18.3',
+        'moviepy==1.0.3',
     ],
 )
